17f3f0de6de10381fdeb10db93c0e343cf1bcfc6
Salome-Serna-R <124018168+Salome-Serna-R@users.noreply.github.com>
2024-09-05 10:41:54 -0500

    Correción de un error

--- appointment/forms.py
+++ appointment/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Appointment
+from .models import Appointment, Reminder
+
 
 #Form to take the appointment details
 class AppointmentForm(forms.ModelForm):
