c8ad1acec28880b5b381b0427160484c15606bc2
nruizu <nruizu@eafit.edu.co>
2024-09-07 10:30:11 -0500

    appointment reminders

--- appointment/views.py
+++ appointment/views.py
@@ -1,19 +1,69 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import AppointmentForm
+from .models import AvailableAppointment
+import os.path
+import datetime as dt
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from dateutil import parser
 
 # Create your views here.
 
 #Function to read the form and save the data in the database
 def form(request):
+    appointments = AvailableAppointment.objects.filter(available=True)
+    SCOPES = ["https://www.googleapis.com/auth/calendar"]
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
         if form.is_valid():
             form.save()
+
+            creds = None
+
+            if os.path.exists("token.json"):
+                creds = Credentials.from_authorized_user_file("token.json")
+
+            if not creds or not creds.valid:
+                if creds and creds.expired and creds.refresh_token:
+                    creds.refresh(Request())
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file("client_secret_158471176816-dc1tg1eho7hbef8ehsu6rgpv7tucnp13.apps.googleusercontent.com.json", SCOPES)
+                    creds = flow.run_local_server(port=0)
+
+                with open("token.json", "w") as token_file:
+                    token_file.write(creds.to_json())
+
+            service = build("calendar", "v3", credentials=creds)
+
+            timeDateTime = parser.isoparse(form.cleaned_data['appointment_date'])
+            endtime = timeDateTime + dt.timedelta(hours=1)
+            endtime = endtime.isoformat()
+            event = {
+                "summary": "Mental health appointment",
+                "start": {"dateTime": form.cleaned_data['appointment_date'], "timezone": "GMT-5"},
+                "end": {"dateTime": endtime, "timezone": "GMT-5"},
+                "reminders" : {
+                    'useDefault': False,
+                    'overrides' : [
+                        {'method': 'email', 'minutes': 24 * 60},
+                        {'method': 'email', 'minutes': 10},
+                        {'method': 'popup', 'minutes': 10}
+                    ]
+                }
+            }
+
+            event = service.events().insert(calendarId=form.cleaned_data['patient_email'], body=event, sendUpdates='all').execute()
+            
             return redirect('success_form')
+        else:
+            print(form.errors)
     else:
         form = AppointmentForm()
-    return render(request, 'fill_appointment.html', {'form': form})
+    return render(request, 'fill_appointment.html', {'form': form, 'appointments': appointments})
 
 
 #function to render the success page after the form is filled
